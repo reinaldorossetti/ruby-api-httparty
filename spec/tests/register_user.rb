5b5bf358f20eb83726c8edbcc760cf3cbf5797f8
reinaldorossetti <reinaldo.rossetti@outlook.com>
2021-07-04 13:56:53 -0300

    melhorando o json schema

--- spec/tests/register_user.rb
+++ spec/tests/register_user.rb
@@ -1,10 +1,22 @@
 RSpec.describe do
+
   it 'Cadastrar usuário de teste' do
     p @body = { "nome": User.full_name, "email": User.email, "password": User.password, "administrador": "true" }
               .to_json
     @response = Service.post('/usuarios', body: @body)
     expect(@response.code).to eq 201
-    p $schema_register, @response.body
-    JSON::Validator.validate!(@response.body, $schema_register, strict: true)
+    p @response.body
+    p schema = JSON.parse(File.read($schema_json_register))
+    JSON::Validator.validate!(schema, @response.body, strict: true)
   end
+
+  it 'Cadastrar usuário de teste negativo' do
+    p @body = { "nome": User.full_name, "email": User.email, "password": User.password, "administrador": "true" }
+              .to_json
+    @response = Service.post('/usuarios', body: @body)
+    expect(@response.code).to eq 201
+    p schema = JSON.parse(File.read($schema_json_user))
+    JSON::Validator.validate!(schema, @response.body, strict: true)
+  end
+
 end
